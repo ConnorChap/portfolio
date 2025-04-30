9f13443417c454f321b7bde8582d602b81900eb4
ConnorChap <connorchap2004@gmail.com>
2025-04-30 00:43:55 -0600

    updated links

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -11,7 +11,7 @@ export const NavBar = ({ menuOpen, setMenuOpen }) => {
             <div className="max-w-5xl mx-auto px-4">
                 <div className="flex justify-between items-center h-16">
                     <a href="#home" className="font-mono text-xl font-bold text-white">
-                        PLACEHOLDERHOME<span className="text-blue-500">.TEST</span>
+                        Connor<span className="text-blue-500"> Chapman</span>
                     </a>
 
                     <div className="w-7 h-5 relative cursor-pointer z-40 md:hidden" onClick={() => setMenuOpen((prev) => !prev)}>
